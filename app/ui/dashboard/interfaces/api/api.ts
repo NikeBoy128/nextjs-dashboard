ba30056810221ddf68aeb6145542905c014a4d47
NikeBoy128 <haroldmosquera739@gmail.com>
2024-05-28 12:59:21 -0500

    chore: Update API base URL to use a variable

--- app/ui/dashboard/interfaces/api/api.ts
+++ app/ui/dashboard/interfaces/api/api.ts
@@ -1,8 +1,8 @@
 import { Data } from "../userlist";
-
+const base_url = '10.10.10.3:3001';
 export async function getPaginatedUsers({ order, page, perPage,roleId,search }: { order: string; page: number; perPage: number,roleId?:number,search?:string}): Promise<Data> {
     
-    const res = await fetch(`http://localhost:3001/user/users-whit-pagination?order=${order}&page=${page}&perPage=${perPage}&search=${search}`);
+    const res = await fetch(`http://${base_url}/user/users-whit-pagination?order=${order}&page=${page}&perPage=${perPage}&search=${search}`);
     const data = await res.json();
     return{
         data: data.data.data,
