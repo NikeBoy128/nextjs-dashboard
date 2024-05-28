2e7f6839f75a83020338d5231a82c046c1f87e35
NikeBoy128 <haroldmosquera739@gmail.com>
2024-05-28 13:01:56 -0500

    chore: Remove unused API call in ListUserPage

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,8 +1,7 @@
 import UserTable from "@/app/ui/dashboard/userTable";
 
 export default async function ListUserPage(){
-    const res= await fetch('http://localhost:3001/user/users-whit-pagination?order=ASC&page=2&perPage=10')
-    const data= await res.json()
+    
     return (
         <div>
         <UserTable/>
